0f759608be253acd321b289dee61ad9310db8de0
arnolec <arno.lecrivain@insa-rennes.fr>
2024-10-14 10:37:23 +0200

    Python file precision

--- euler.py
+++ euler.py
@@ -1,10 +1,13 @@
+from decimal import *
+from mpmath import mp
 def main():
-    e = 2.71828
-    investissement = 0
-    for i in range(50) : 
-        e -=1 
-        investissement += 1 * i
-    print(investissement)
-    # return investissement
+    mp.dps = 100
+    e = mp.mpf('2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274')
+    for i in range(1,50) : 
+        e = mp.mpf(e-1)*i
+        print(f"i : {i} , e :{e}")
+    return e
 
-main()
+if __name__ == "__main__":
+    main()
+
