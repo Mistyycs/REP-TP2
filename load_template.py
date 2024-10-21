54c8ac0ccbf07b28fda833eb9c807dbe5bc564d2
Jérémy Brunet <jeremy.brunet@insa-rennes.fr>
2024-10-21 10:50:33 +0200

    Ajout load_template

---
+++ load_template.py
@@ -0,0 +1,17 @@
+from jinja2 import Template
+
+template_content = open('property.py.jinja').read()
+template = Template(template_content)
+
+factors = {
+    "operation1" : "(x+y) + z",
+    "operation2" : "x+ (y+z)",
+    "repetitions" : 1000,
+}
+
+generated_code = template.render(factors)
+
+with open('generated_property_check.py','w') as f:
+    f.write(generated_code)
+
+printf("Generated code has been written to 'generated_property_check.py'.")
